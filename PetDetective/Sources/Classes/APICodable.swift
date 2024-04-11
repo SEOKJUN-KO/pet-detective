2f5dbd4e754c73839b6736ddbfdbdc31db50d658
SEOKJUN KO <64264896+SEOKJUN-KO@users.noreply.github.com>
2024-04-11 11:24:38 +0900

    Update APICodable.swift

--- PetDetective/Sources/Classes/APICodable.swift
+++ PetDetective/Sources/Classes/APICodable.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+// 왜 class로 코더블을 했을까
+// 단순히 변수만 저장하고 있기 때문에, struct든 class든 성능차이는 크게 나지 않을 것 같음
+// 다만 복사가 되는 경우, 참조 값을 사용할 것인지, 새 변수를 사용할 것인지 고려해볼 부분인 것 같고
+// 값의 불변성이 높고 공유 가능성이 낮고 상속이 필요하지 않기 때문에, 일반적으로 사용하는 struct를 사용했어도 좋았을 것 같다.
 class ReportBoard: Codable{
     let mainImageUrl: String?
     let missingLocation: String?
@@ -40,6 +44,7 @@ class FindBoard: Codable{
 
 class APIFinderBoardResponse<T: Codable> : Codable {
     var totalPage: Int?
+    // FindBoard를 담는 배열 형태가 명확함으로 Generic으로 꼭 사용할 필요는 없었을 듯
     var finderBoardDTOS: T?
 }
 
@@ -106,4 +111,3 @@ struct PutWithoutImage:Encodable{
     let missingLongitude: Double
     let missingLatitude: Double
 }
-
